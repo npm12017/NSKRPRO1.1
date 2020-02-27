abb9043ba3127fe34299828c3a0c4bbd62fdf28e
npm12017 <nazim.mer@gmail.com>
2020-02-27 08:43:17 +0000

    update watch_temp &protection Bed  settings

--- 7dec2019/Marlin-2.0.x/Marlin/Configuration_adv.h
+++ 7dec2019/Marlin-2.0.x/Marlin/Configuration_adv.h
@@ -157,15 +157,15 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD 60 //20                // Seconds 27/12/2019
-  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
+  #define WATCH_TEMP_PERIOD 20 //20                // Seconds 27/12/2019
+  #define WATCH_TEMP_INCREASE 2              // Degrees Celsius
 #endif
 
 /**
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD 20    // Seconds
+  #define THERMAL_PROTECTION_BED_PERIOD 50   // Seconds
   #define THERMAL_PROTECTION_BED_HYSTERESIS 2 // Degrees Celsius
 
   /**
